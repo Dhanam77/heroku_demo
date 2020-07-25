60cfad64d3f9cec99f1e50fe5a25d0578568ad02
Dhanam <42117184+Dhanam77@users.noreply.github.com>
2020-07-25 11:30:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,26 +8,17 @@ Created on Sat Jun 13 15:44:37 2020
 from flask import Flask
 from flask import jsonify, request
 app = Flask(__name__)
-
 from nlp_twitter import getSentiment
 
 @app.route("/sentiment", methods = ['POST'])
 def get_sentiment():
-
     json = request.json
-
     if json == None:
         return jsonify({"null":"Enter valid input"})
-
     for x in json:
         s = json[x]
-
     z = getSentiment(s)
     y = {"sentiment": str(z)}
-
     return jsonify(y)
-
-
-
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader = False)
+    app.run()
